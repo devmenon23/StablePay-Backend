d963f4edf5e7d91d9716f1a26799bc8019e404cb
michael20242025 <nwaigwe@wisc.edu>
2025-11-22 17:35:34 -0600

    graph update

--- src/structure/graph.py
+++ src/structure/graph.py
@@ -4,23 +4,40 @@ class Edge:
         self.to_node = to_node
         self.cost = cost
         self.exchange = exchange
+    
     def __str__(self):
-        return "edge: exchange [%s] to [%s] cost [%s]" % (self.exchange, self.to_node.name, self.cost)
+        return "edge: exchange [%s] to [%s] cost [%s]" % (
+            self.exchange, self.to_node.name, self.cost
+        )
+
 
 class Node:
     def __init__(self, name):
         self.name = name
         self.edges = []
+
     def __str__(self):
-        return "node: name %s edges: [%s]" % (self.name, ', '.join([str(edge) for edge in self.edges]))
+        return "node: name %s edges: [%s]" % (
+            self.name, ', '.join([str(edge) for edge in self.edges])
+        )
+
 
 def get_cost(from_node, to_node):
-    # returns cost, exchange
-    # not yet implemented
+    # placeholder
     return 67, "transact"
 
+
 def get_neighbors(currency):
-    # returns list of currencies this currency can be converted to
+    # placeholder – replace with API-based neighbors later
+    neighbors = {
+        "USDC": ["USD", "ARS", "SOL", "BTC"],
+        "USD":  ["USDC", "ARS"],
+        "ARS":  ["USDC", "USD"],
+        "SOL":  ["USDC"],
+        "BTC":  ["USDC"],
+    }
+    return neighbors.get(currency, [])
+
 
 class Graph:
     def __init__(self):
@@ -30,33 +47,43 @@ class Graph:
             Node("USD"),
             Node("SOL"),
             Node("BTC"),
-            ]
-            
-            self.nametonode = {
-            Node("USDC"):0,
-            Node("ARS"):1,
-            Node("USD"):2,
-            Node("SOL"):3,
-            Node("BTC"):4,
-            }
-    def add_edge(self, from_node, edge):
-        self.nodes[from_node].edges.append(edge)
+        ]
+
+        self.nametoindex = {
+            "USDC": 0,
+            "ARS": 1,
+            "USD": 2,
+            "SOL": 3,
+            "BTC": 4,
+        }
+
+    def add_edge(self, from_index, edge):
+        self.nodes[from_index].edges.append(edge)
+
     def update_costs(self):
         for node in self.nodes:
             for edge in node.edges:
                 edge.cost, edge.exchange = get_cost(node, edge.to_node)
-    def links(self, node):
-        # returns list of nodes this node should be connected to
-        currencies = get_neighbors(currency)
-        for currency in currencies:
-            if not 
-        
 
-def link(graph):
-    for node in self.nodes:
-        
-        graph.add_edge(node, )
+    def setup_links(self):
+        for node in self.nodes:
+            currencies = get_neighbors(node.name)
 
+            # current node index
+            from_index = self.nametoindex[node.name]
+
+            for currency in currencies:
+                if currency not in self.nametoindex:
+                    continue
+
+                to_index = self.nametoindex[currency]
+                to_node = self.nodes[to_index]
+
+                # cost initially 0, exchange empty
+                self.add_edge(
+                    from_index,
+                    Edge(to_node, 0, "")
+                )
 
 g = Graph()
 print(g.nodes[1])
