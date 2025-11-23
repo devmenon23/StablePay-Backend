530d460b1b1d19f5eee61d62d76111a16a110a86
devmenon23 <68741023+devmenon23@users.noreply.github.com>
2025-11-23 01:50:03 -0600

    Endpoints added

--- src/exchange_crypto.py
+++ src/exchange_crypto.py
@@ -36,34 +36,7 @@ COINGECKO_IDS = {
     "MANA": "decentraland",
 }
 
-def Get_cost(src, dest, amount): # amount is in src
-    # curl --location 'https://api.swapzone.io/v1/exchange/get-rate?from=btc&to=doge&amount=0.1&rateType=all&availableInUSA=false&chooseRate=best&noRefundAddress=false&ofcAdapter=true' --header 'x-api-key: w7vDUCFZQ'
-    url = "https://api.swapzone.io/v1/exchange/get-rate"
-
-    params = {
-        "from": src.lower(),
-        "to": dest.lower(),
-        "amount": amount,
-        "rateType": "all",
-        "availableInUSA": "false",
-        "chooseRate": "best",
-        "noRefundAddress": "false",
-        "ofcAdapter": "true"
-    }
-
-    headers = {
-        "x-api-key": "w7vDUCFZQ"
-    }
-
-    response = requests.get(url, params=params, headers=headers)
-    if response.status_code != 200:
-        raise ValueError("Failed to fetch cost from swapzone")
-    resp = response.json()
-    before_usd = convert_to_usd(COINGECKO_IDS[src], resp['amountFrom'])
-    after_usd = convert_to_usd(COINGECKO_IDS[dest], resp['amountTo'])
-    return before_usd - after_usd, "swapzone"
-
-def convert_usd_crypt(currency_symbol, amount):
+def convert_to_usd(currency_symbol, amount):
     """
     Convert `amount` of crypto `currency_symbol` to USD using CoinGecko.
     Example: convert_to_usd("btc", 0.05)
@@ -91,5 +64,32 @@ def convert_usd_crypt(currency_symbol, amount):
 
     price_usd = data[currency_symbol.lower()]["usd"]
     return amount * price_usd
+
+def Get_cost(src, dest, amount): # amount is in src
+    # curl --location 'https://api.swapzone.io/v1/exchange/get-rate?from=btc&to=doge&amount=0.1&rateType=all&availableInUSA=false&chooseRate=best&noRefundAddress=false&ofcAdapter=true' --header 'x-api-key: w7vDUCFZQ'
+    url = "https://api.swapzone.io/v1/exchange/get-rate"
+
+    params = {
+        "from": src.lower(),
+        "to": dest.lower(),
+        "amount": amount,
+        "rateType": "all",
+        "availableInUSA": "false",
+        "chooseRate": "best",
+        "noRefundAddress": "false",
+        "ofcAdapter": "true"
+    }
+
+    headers = {
+        "x-api-key": "w7vDUCFZQ"
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        raise ValueError("Failed to fetch cost from swapzone")
+    resp = response.json()
+    before_usd = convert_to_usd(COINGECKO_IDS[src], resp['amountFrom'])
+    after_usd = convert_to_usd(COINGECKO_IDS[dest], resp['amountTo'])
+    return before_usd - after_usd, "swapzone"
 
 #print(Get_cost("SOL", "WBTC", 20))
